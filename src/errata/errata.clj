6f686e234830aca3ff06a23f12ae03d5b162adbb
David A. Viramontes <dviramontes@gmail.com>
2021-02-07 18:21:08 -0700

    lib init

---
+++ src/errata/errata.clj
@@ -0,0 +1,6 @@
+(ns errata.errata)
+
+(defn init
+  "Initialize errata"
+  [x]
+  (prn x "It works!"))
---
+++ test/errata/errata_test.clj
@@ -0,0 +1,7 @@
+(ns errata.errata-test
+  (:require [clojure.test :refer :all]
+            [errata.errata :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
